a5e85a5e056c03178e064969c657479ee35dbd22
Jeremy Hermelin <contact.hermelin@gmail.com>
2020-01-14 01:00:57 +0100

    inventory model bug fixed

--- models/inventory.js
+++ models/inventory.js
@@ -11,19 +11,22 @@ let inventorySchema = new Schema([{
 
 const isSlotAvailableForItem = (slot, item) => {
   // empty slot
-  if (!slot._id || (slot.resourceId && slot.qty === 0)) return true;
-  
+  if (!slot.name && !slot.resourceId && !slot.qty) {
+    return true;
+  }
   // slot with same resource
   if (slot.resourceId && item.resourceId && item.resourceId.toString() === slot.resourceId.toString()) {
     return true;
   }
-
   // Slot not available for this item
   return false;
 };
 
 const updateSlotFromItem = (slot, item) => {
   if (item.resourceId && item.qty) {
+    if (!slot.resourceId) {
+      return item;
+    }
     if (slot.resourceId.toString() !== item.resourceId) {
       throw new Error('trying to mix different resources together');
     }
@@ -35,36 +38,43 @@ const updateSlotFromItem = (slot, item) => {
   }
 };
 
-inventorySchema.methods.addItem = function(item) {
+inventorySchema.methods.addItem = async function(item) {
   if (this.inventory.length < MAX_AVAILABLE_SLOTS) {
     this.inventory.push(newItem);
-    return this.save();
+    this.markModified('inventory');
+    return await this.save();
   }
+  // FIXME: instead of finding first slot available, need to find best slot available (merging same resource together, even if there are free slots)
   const indexToModify = this.inventory.findIndex(slot => isSlotAvailableForItem(slot, item));
-  if (!indexToModify) {
+  if (indexToModify === -1) {
     throw new Error('inventory full');
   }
   this.inventory[indexToModify] = updateSlotFromItem(this.inventory[indexToModify], item);
-  return this.save();
+  this.markModified('inventory');
+  return await this.save();
 };
 
 
-inventorySchema.methods.removeItem = function({ resourceId, qty, _id }) {
+inventorySchema.methods.removeItem = async function({ resourceId, qty, _id }) {
   if (_id) {
-    // Simply remove the provided id
-    this.inventory.splice(this.inventory.findIndex(slot => slot._id === _id), 1);
-    return this.save();
+    // Simply replace the provided id    
+    this.inventory[this.inventory.findIndex(slot => slot._id.toString() === _id)] = {
+      resourceId: null,
+      qty: null,
+      type: null,
+    };
+    this.markModified('inventory');
+    return await this.save();
   }
   if (resourceId && qty) {
     let remainingQty = qty;
     for (let i = 0; i < this.inventory.length; i++) {
       const slot = this.inventory[i];
       // Slot is of same resource
-      if (slot.resourceId.toString() === resourceId.toString()) {
+      if (slot.resourceId && slot.resourceId.toString() === resourceId.toString()) {
         const qtyToRemove = Math.min(slot.qty, remainingQty);
         remainingQty -= qtyToRemove;
         slot.qty -= qtyToRemove;
-
         if (slot.qty === 0) {
           slot.resourceId = null;
         }
@@ -75,7 +85,8 @@ inventorySchema.methods.removeItem = function({ resourceId, qty, _id }) {
     if (remainingQty) {
       throw new Error('not enough qty available');
     }
-    return this.save();
+    this.markModified('inventory');
+    return await this.save();
   }
   // else cannot remove
   throw new Error('cannot remove');
